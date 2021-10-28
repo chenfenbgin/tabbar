cfc3c148974965e2c29d0766aa1662ff7e3204af
chenfengbin <1838272147@qq.com>
2021-10-28 21:12:35 +0800

    photo init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <!-- 在没有封装之前，我们一般是这么做的 -->
-    <div id="tar-bar">
-      <div class="tar-bar-item">首页</div>
-      <div class="tar-bar-item">分类</div>
-      <div class="tar-bar-item">购物车</div>
-      <div class="tar-bar-item">我的</div>
-    </div>
+    <tar-bar></tar-bar>
   </div>
 </template>
 
 <script>
+// 导入组件之后，需要在components中进行注册，然后在使用标签
+import TarBar from "../src/components/tabbar/TabBar.vue"  
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    TarBar
+  },
 };
 </script>
 
@@ -22,23 +21,26 @@ export default {
 /* 在style中引用，需要加一个@import */
 
 @import "./assets/css/base.css";
-#tar-bar {
-  display: flex;
-  background-color: #f6f6f6;
 
-  position: fixed; /* 跑到最下面 */
-  left: 0;
+/* 此处样式移到App.vue中 */
+/* #tar-bar {
+  display: flex;
+  background-color: #f6f6f6; */
+
+/* 元素的位置相对于浏览器窗口是固定位置。即使窗口是滚动的它也不会移动： */
+  /* position: fixed; /* 跑到最下面 */
+  /* left: 0;
   right: 0;
-  bottom: 0;
+  bottom: 0; */
 
   /* 设置阴影, 第一个和第二个值，代表x、y轴，如果都为正，往第四象限走 */
-  box-shadow: 0 -1px 1 rgba(100, 100, 100, 2);
+  /* box-shadow: 0 -1px 1 rgba(100, 100, 100, 2);
 }
 
-.tar-bar-item {
+.tar-bar-item { */
   /* flex:1; 就是代表均匀分配元素 */
-  flex: 1;
+  /* flex: 1;
   text-align: center;
   height: 49px; /* 一般tabbar高度就是49px,ios安卓都是 */
-}
+/* } */
 </style>
---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <!-- 在没有封装之前，我们一般是这么做的,没有复用性 -->
+    <div id="tar-bar">
+      <div class="tar-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" />
+        首页
+      </div>
+      <div class="tar-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" />
+        分类
+      </div>
+      <div class="tar-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" />
+        购物车
+      </div>
+      <div class="tar-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" />
+        我的
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "TarBar",
+  data() {
+    return {};
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+#tar-bar {
+  display: flex;
+  background-color: #f6f6f6;
+
+  /* 元素的位置相对于浏览器窗口是固定位置。即使窗口是滚动的它也不会移动： */
+  position: fixed; /* 跑到最下面 */
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  /* 设置阴影, 第一个和第二个值，代表x、y轴，如果都为正，往第四象限走 */
+  box-shadow: 0 -1px 1 rgba(100, 100, 100, 2);
+}
+
+.tar-bar-item {
+  /* flex:1; 就是代表均匀分配元素 */
+  flex: 1;
+  text-align: center;
+  height: 49px; /* 一般tabbar高度就是49px,ios安卓都是 */
+}
+</style>
